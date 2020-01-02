cd2def0843f60ca63b7ad66a2a6da6a4f0d9b8d4
Nep T. Orshiso <nep.orshiso@gmail.com>
2020-01-02 18:32:48 -0500

    juggling async complete

---
+++ juggling-async.js
@@ -0,0 +1,26 @@
+/* Juggling Async (Exercise 9 of 13)
+This problem is the same as the previous problem (HTTP COLLECT) in that  
+  you need to use http.get(). However, this time you will be provided with  
+  three URLs as the first three command-line arguments.  
+*/
+
+const http = require("http");
+const concat = require("concat-stream");
+const urls = process.argv.slice(2);
+const ans = [];
+let count = urls.length;
+
+urls.forEach((url, index) => {
+  http.get(url, response => {
+    response.setEncoding("utf8");
+    response.pipe(
+      concat(data => {
+        ans[index] = data.toString();
+        count--;
+        if (count == 0) {
+          ans.forEach(res => console.log(res));
+        }
+      })
+    );
+  });
+});
